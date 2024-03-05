c008f675c57690294234ef6a06d9d395f049e6e1
Joshua <111794087+jmayores@users.noreply.github.com>
2024-03-04 21:41:49 -0600

    Add UI functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# odin-rock-paper-scissors
+# Odin Project: Rock, Paper, Scissors
+
+This is a project I have completed from the Odin Project, with the goal of using Javascript to create a rock, paper, scissors game.
--- script.js
+++ script.js
@@ -1,34 +1,74 @@
+let sel = document.querySelector("#selections");
+let results = document.querySelector("#results");
+let playerChoice = document.querySelector("#player-choice");
+let computerChoice = document.querySelector("#computer-choice");
+let playerScore = document.querySelector("#player-score");
+let computerScore = document.querySelector("#computer-score");
+let pScore = 0;
+let cScore = 0;
+let result;
+
+playerScore.textContent = pScore;
+computerScore.textContent = cScore;
+
+getPlayerChoice = (event) => {
+  let target = event.target;
+
+  switch (target.id) {
+    case "rock":
+      results.textContent = playRound("rock", getComputerChoice());
+      break;
+    case "paper":
+      results.textContent = playRound("paper", getComputerChoice());
+      break;
+    case "scissors":
+      results.textContent = playRound("scissors", getComputerChoice());
+      break;
+  }
+};
+
+sel.addEventListener("click", getPlayerChoice);
+
 getComputerChoice = () => {
-  const options = ["Rock", "Paper", "Scissors"];
+  const options = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * options.length);
   return options[randomIndex];
 };
 
 playRound = (playerSelection, computerSelection) => {
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
+  playerChoice.textContent = playerSelection;
+  computerChoice.textContent = computerSelection;
 
   if (playerSelection === computerSelection)
-    return `It's a tie! You both used ${playerSelection}...`;
+    result = `It's a tie! You both used ${playerSelection}...`;
   else if (playerSelection === "rock") {
-    if (computerSelection === "paper") return "You lose! Paper beats rock...";
-    else return "You won! Rock beats paper!";
+    if (computerSelection === "paper") result = "You lose! Paper beats rock...";
+    else result = "You win! Rock beats paper!";
   } else if (playerSelection === "paper") {
     if (computerSelection === "scissors")
-      return "You lose! Scissor beats paper...";
-    else return "You win! Paper beats rock!";
+      result = "You lose! Scissor beats paper...";
+    else result = "You win! Paper beats rock!";
   } else if (playerSelection === "scissors") {
-    if (computerSelection === "rock") return "You lose! Rock beats scissors...";
-    else return "You win! Scissors beats paper!";
-  } else
-    return `Do you even know how to play rock, paper, scissors??? You have to choose either rock, paper, or scissors... not ${playerSelection}`;
-};
-
-playGame = () => {
-  for (i = 0; i < 5; i++) {
-    playerSelection = prompt("Choose rock, paper, or scissors!", "Rock");
-    console.log(playRound(playerSelection, getComputerChoice()));
+    if (computerSelection === "rock")
+      result = "You lose! Rock beats scissors...";
+    else result = "You win! Scissors beats paper!";
   }
-};
 
-playGame();
+  if (result.substring(0, 7) === "You win") {
+    pScore++;
+    playerScore.textContent = pScore;
+  } else if (result.substring(0, 8) === "You lose") {
+    cScore++;
+    computerScore.textContent = cScore;
+  }
+
+  if (pScore === 5) {
+    result = "You got 5 points! You won the game!";
+    sel.removeEventListener("click", getPlayerChoice);
+  } else if (cScore === 5) {
+    result = "The computer got 5 points... You lost the game!";
+    sel.removeEventListener("click", getPlayerChoice);
+  }
+
+  return result;
+};
